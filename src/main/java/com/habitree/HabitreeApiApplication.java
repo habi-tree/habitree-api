a2260d24f65ee679573d4e86f74ccc9df03892f9
Seungwon-Choi <bestwondev@gmail.com>
2025-05-25 17:41:11 +0900

    initial commit

---
+++ src/main/java/com/habitree/HabitreeApiApplication.java
@@ -0,0 +1,13 @@
+package com.habitree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitreeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitreeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/habitree/HabitreeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitreeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
